3c211aead2552c72d0521e7e47d2b51838c1c152
blackmarimo <qqmn53f9k@gmail.com>
2015-11-23 22:28:20 +0900

    コミット

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/ 
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+        ////
+        //// GET: /HelloWorld/ 
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //}
+
+        ////
+        //// GET: /HelloWorld/Welcome/ 
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID is: " + ID);
+        //}
+    }
+}
