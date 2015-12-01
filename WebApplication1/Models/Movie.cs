2601defd91c47df98ab4a5f4ce80f059a4a765a0
blackmarimo <qqmn53f9k@gmail.com>
2015-12-01 23:27:03 +0900

    コミット

--- WebApplication1/Models/Movie.cs
+++ WebApplication1/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace WebApplication1.Models
@@ -7,6 +8,10 @@ namespace WebApplication1.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "リリース日")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
